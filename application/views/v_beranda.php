548f23301891839a4f2a0f0d87e34ea3449fe1d2
Kartika Sari <kartika.sari.tif16@polban.ac.id>
2018-11-26 23:49:01 +0700

    Update v_beranda.php

--- application/views/v_beranda.php
+++ application/views/v_beranda.php
@@ -1,64 +1,72 @@
-<div class="container">
-	<div class="beranda">
-		<h4>Nama Obat Mirip</h4>
-		<div class="col-lg-2 col-md-6 mb-3">
-			<img class="img-thumbnail" src="<?php echo base_url().'assets/images/Chlorpromazine.jpg'?>">
-				Chlorpromazine
-		</div>
-		<div class="col-lg-2 col-md-6 mb-3">
-			<img class="img-thumbnail" src="<?php echo base_url().'assets/images/Chlorpromazine.jpg'?>">
-				Chlorpromazine
-		</div>
-		<div class="col-lg-2 col-md-6 mb-3">
-			<img class="img-thumbnail" src="<?php echo base_url().'assets/images/Chlorpromazine.jpg'?>">
-				Chlorpromazine
-		</div>
-		<div class="col-lg-2 col-md-6 mb-3">
-			<img class="img-thumbnail" src="<?php echo base_url().'assets/images/Chlorpromazine.jpg'?>">
-				Chlorpromazine
-		</div>
-		<div class="col-lg-2 col-md-6 mb-3">
-			<img class="img-thumbnail" src="<?php echo base_url().'assets/images/Chlorpromazine.jpg'?>">
-				Chlorpromazine
-		</div>
-		<div class="col-lg-2 col-md-6 mb-3">
-			<img class="img-thumbnail" src="<?php echo base_url().'assets/images/Chlorpromazine.jpg'?>">
-				Chlorpromazine
-		</div>
-	</div>
-<br><br>
-	<div class="beranda">
-		<h4>Kemasan Mirip</h4>
-		<div class="col-lg-2 col-md-6 mb-3">
-			<img class="img-thumbnail" src="<?php echo base_url().'assets/images/Chlorpromazine.jpg'?>">
-				Chlorpromazine
-		</div>
-		<div class="col-lg-2 col-md-6 mb-3">
-			<img class="img-thumbnail" src="<?php echo base_url().'assets/images/Chlorpromazine.jpg'?>">
-				Chlorpromazine
-		</div>
-		<div class="col-lg-2 col-md-6 mb-3">
-			<img class="img-thumbnail" src="<?php echo base_url().'assets/images/Chlorpromazine.jpg'?>">
-				Chlorpromazine
-		</div>
-		<div class="col-lg-2 col-md-6 mb-3">
-			<img class="img-thumbnail" src="<?php echo base_url().'assets/images/Chlorpromazine.jpg'?>">
-				Chlorpromazine
-		</div>
-		<div class="col-lg-2 col-md-6 mb-3">
-			<img class="img-thumbnail" src="<?php echo base_url().'assets/images/Chlorpromazine.jpg'?>">
-				Chlorpromazine
-		</div>
-		<div class="col-lg-2 col-md-6 mb-3">
-			<img class="img-thumbnail" src="<?php echo base_url().'assets/images/Chlorpromazine.jpg'?>">
-				Chlorpromazine
-		</div>
-	</div>
 
-	<?php
-		foreach($list_golongan as $row) 
-		{
-			echo $row['nama_golongan'];
-		}
-	?>
-</div>
+  <body>
+
+    <main role="main">
+
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item">
+            <img class="first-slide" src="<?php echo base_url('assets/images/medicine.png')?>" alt="First slide">
+            <div class="container">
+              <div class="carousel-caption text-right">
+                <h1 color="black">NORUM</h1>
+                <h3>Nama Obat Rupa dan Ucapan Mirip</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <!-- Marketing messaging and featurettes
+      ================================================== -->
+      <!-- Wrap the rest of the page in another container to center all the content. -->
+
+      <div class="container marketing">
+
+        <div class="row featurette">
+          <div class="col-md-12">
+            <h3 class="featurette-heading"><span class="text-muted">Standar Pelayanan Kefarmasian di Rumah Sakit</span></h3><br>
+            <p class="lead" align="justify">Dalam Permenkes no 72 tahun 2016, Rumah Sakit perlu mengembangkan kebijakan pengelolaan Obat untuk meningkatkan keamanan, khususnya Obat yang perlu diwaspadai (high- alert medication). High-alert medication adalah Obat yang harus diwaspadai karena sering menyebabkan terjadi kesalahan/kesalahan serius (sentinel event) dan Obat yang berisiko tinggi menyebabkan Reaksi Obat yang Tidak Diinginkan (ROTD). Salah satu kelompok obat high-alert yaitu Obat yang terlihat mirip dan kedengarannya mirip (Nama Obat Rupa dan Ucapan Mirip/NORUM, atau Look Alike Sound Alike/LASA).</p>
+          </div>
+        </div>
+
+		<!--
+        <hr class="featurette-divider">
+		<div class="row">
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="<?php echo base_url('assets/images/farmasi.jpg')?>" alt="Generic placeholder image" width="140" height="140">
+            <h2>Heading</h2>
+            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div>
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="<?php echo base_url('assets/images/farmasi.jpg')?>" alt="Generic placeholder image" width="140" height="140">
+            <h2>Heading</h2>
+            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div>
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="<?php echo base_url('assets/images/farmasi.jpg')?>" alt="Generic placeholder image" width="140" height="140">
+            <h2>Heading</h2>
+            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div>
+        </div>
+		-->
+
+
+      </div><!-- /.container -->
+    </main>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="<?php echo base_url('assets/bootstrap/js/popper.min.js')?>"></script>
+    <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js')?>"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="<?php echo base_url('assets/bootstrap/js/vendor/holder.min.js')?>"></script>
+  </body>
+</html>
